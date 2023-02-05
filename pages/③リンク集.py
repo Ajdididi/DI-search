2e4da0e87415f6e8396ebaa81f16e22e14a8cd00
Ajdididi <anjokosei.druginfo@gmail.com>
2023-02-05 14:19:55 +0900

    first upload

---
+++ pages/③リンク集.py
@@ -0,0 +1,47 @@
+import streamlit as st
+st.title('リンク集')
+st.write('---------------------------------')
+st.write('■ DI作成資料')
+st.write('添付文書改訂情報（Googleドライブ） \n'
+        + '((作成中))')
+st.write('フォーミュラリ#37（Googleドライブ）  \n'
+        + 'https://drive.google.com/drive/folders/1ereCyYVwErOFm0i2HmtPwKHv_3uGljvA?usp=sharing')
+st.write('相互作用エラー設定（Googleドライブ）  \n'
+        + '- ア～サ https://drive.google.com/drive/folders/1-779eNFw_1RWiizxrRug4etLGmJ2qDJZ?usp=sharing  \n'
+        + '- タ～ハ https://drive.google.com/drive/folders/1uesthl3SENipWn4asIlHtV71O72Qq2G0?usp=sharing  \n'
+        + '- マ～ワ https://drive.google.com/drive/folders/1I6uv4SrbhDHv_Ow0Dkp4yN8bafkp07Jg?usp=sharing')
+st.write('---------------------------------')
+st.write('■ 医薬品・健康食品')
+st.write('PMDA医療用医薬品 情報検索  \n'
+        + 'https://www.pmda.go.jp/PmdaSearch/iyakuSearch/')
+st.write('SAFE-DI  \n'
+        + 'https://www.safe-di.jp/')
+st.write('「健康食品」の安全性・有効性情報  \n'
+        + 'https://hfnet.nibiohn.go.jp/')
+
+st.write('---------------------------------')
+st.write('■ 文献検索')
+st.write('メディカルオンライン  \n'
+        + 'https://www.medicalonline.jp/')
+st.write('UpToDate  \n'
+        + 'https://www.uptodate.com/contents/search')
+st.write('PubMed  \n'
+        + 'https://pubmed.ncbi.nlm.nih.gov/')
+st.write('ClinicalKey  \n'
+        + 'https://www.clinicalkey.jp/#!/')
+st.write('LIFE SCIENCE DICTIONARY  \n'
+        + 'https://lsd-project.jp/cgi-bin/lsdproj/ejlookup04.pl')
+
+st.write('---------------------------------')
+st.write('■ ガイドライン・学会HP')
+st.write('NCCN  \n'
+        + 'https://www.nccn.org/')
+st.write('日本化学療法学会-VCM TDMソフトウェア PAT-  \n'
+        + 'https://www.chemotherapy.or.jp/modules/guideline/index.php?content_id=79')
+st.write('日本腎臓病薬物療法学会  \n'
+        + '- 腎機能低下時に最も注意が必要な薬剤投与量一覧 https://www.jsnp.org/ckd/yakuzaitoyoryo.php  \n'
+        + '- eGFR・CCrの計算 https://www.jsnp.org/egfr/')
+st.write('---------------------------------')
+st.write('■ その他')
+st.write('実務実習指導・管理システム  \n'
+        + 'https://training-phm.fujifilm.com/Usersite')
---
+++ ①問合せ記録.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import sqlite3
+from datetime import datetime
+import streamlit as st
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    st.title('問合せ記録 横断検索（DB変換版）')
+    kensaku = st.text_input('検索ワード（1単語のみ）を入力してください。  ※半角・全角は区別されます')
+    btn = st.button('検索')
+
+    st.write('===== データベース使用上の注意 =====')
+    st.write('問合せ記録は、過去の対応事例の内容を示すデータベースです。')
+    st.write('対応時点で根拠とした医薬品情報が現在も同じエビデンスレベルで活用できるとは限らないことにご留意ください。')
+    st.write('※このページからは情報の修正はできません。修正が必要な場合は管理担当者へ連絡してください。')
+
+    if btn:
+        kensaku = '%' + kensaku + '%'
+        db = sqlite3.connect('toiawase.db')
+        cur = db.cursor()    
+
+        cur.execute("SELECT * FROM to_2021 WHERE category LIKE ? OR drug LIKE ? OR question LIKE ? OR answer LIKE ?", 
+                    [kensaku, kensaku, kensaku, kensaku])
+        data = cur.fetchall()
+
+        cur.execute("SELECT * FROM from_2022 WHERE category LIKE ? OR drug LIKE ? OR question LIKE ? OR answer LIKE ? OR reference LIKE ?", 
+                    [kensaku, kensaku, kensaku, kensaku, kensaku])
+        data_n = cur.fetchall()
+
+        st.write('■■■2022年1月以降の問合せ記録: {len(data_n)}件■■■')
+        for i in range(0, len(data_n)):
+            tdatetime = datetime.strptime(data_n[i][0], '%Y-%m-%d %H:%M:%S') 
+            tdate = tdatetime.date().strftime('%Y/%m/%d')
+            st.write('【日付】' + tdate)
+            st.write('【種別】' + data_n[i][1])
+            st.write('【医薬品名】' + data_n[i][2])
+            st.write('【質問】')
+            st.write(data_n[i][3])
+            st.write('【回答】')
+            st.write(data_n[i][4])
+            st.write('【参考文献】')
+            st.write(data_n[i][5])
+            st.write('-------------------------------------------------------------------')
+        st.write('')
+        st.write('■■■2021年12月以前の問合せ記録: {len(data)}件■■■')
+        for i in range(0, len(data)):
+            tdatetime = datetime.strptime(data[i][0], '%Y-%m-%d %H:%M:%S')
+            tdate = tdatetime.date().strftime('%Y/%m/%d')
+            st.write('【日付】' + tdate)
+            st.write('【種別】' + data[i][1])
+            st.write('【医薬品名】' + data[i][2])
+            st.write('【質問】')
+            st.write(data[i][3])
+            st.write('【回答】')
+            st.write(data[i][4])
+            st.write('-------------------------------------------------------------------')
+        cur.close()
+        db.close()
