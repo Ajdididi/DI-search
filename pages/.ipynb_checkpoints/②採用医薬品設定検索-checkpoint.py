e5b57bfa6d7f41c1a6796ffd3c3bf79886c54293
Ajdididi <anjokosei.druginfo@gmail.com>
2023-02-05 14:50:49 +0900

    5th

---
+++ pages/.ipynb_checkpoints/②採用医薬品設定検索-checkpoint.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write('----作成中----')
+st.write('----In the making ----')
--- pages/.ipynb_checkpoints/③リンク集-checkpoint.py
+++ pages/.ipynb_checkpoints/③リンク集-checkpoint.py
@@ -7,9 +7,9 @@ st.write('▸添付文書改訂情報（Googleドライブ）  \n'
 st.write('▸フォーミュラリ#37（Googleドライブ）  \n'
         + 'https://drive.google.com/drive/folders/1ereCyYVwErOFm0i2HmtPwKHv_3uGljvA?usp=sharing')
 st.write('▸相互作用エラー設定（Googleドライブ）  \n'
-        + 'ア～サ https://drive.google.com/drive/folders/1-779eNFw_1RWiizxrRug4etLGmJ2qDJZ?usp=sharing  \n'
-        + 'タ～ハ https://drive.google.com/drive/folders/1uesthl3SENipWn4asIlHtV71O72Qq2G0?usp=sharing  \n'
-        + 'マ～ワ https://drive.google.com/drive/folders/1I6uv4SrbhDHv_Ow0Dkp4yN8bafkp07Jg?usp=sharing')
+        + 'ア～サ： https://drive.google.com/drive/folders/1-779eNFw_1RWiizxrRug4etLGmJ2qDJZ?usp=sharing  \n'
+        + 'タ～ハ： https://drive.google.com/drive/folders/1uesthl3SENipWn4asIlHtV71O72Qq2G0?usp=sharing  \n'
+        + 'マ～ワ： https://drive.google.com/drive/folders/1I6uv4SrbhDHv_Ow0Dkp4yN8bafkp07Jg?usp=sharing')
 st.write('---------------------------------')
 st.write('■ 医薬品・健康食品')
 st.write('▸PMDA医療用医薬品 情報検索  \n'
@@ -39,8 +39,8 @@ st.write('▸NCCN  \n'
 st.write('▸日本化学療法学会-VCM TDMソフトウェア PAT-  \n'
         + 'https://www.chemotherapy.or.jp/modules/guideline/index.php?content_id=79')
 st.write('▸日本腎臓病薬物療法学会  \n'
-        + '腎機能低下時に最も注意が必要な薬剤投与量一覧 https://www.jsnp.org/ckd/yakuzaitoyoryo.php  \n'
-        + 'eGFR・CCrの計算 https://www.jsnp.org/egfr/')
+        + '腎機能低下時に最も注意が必要な薬剤投与量一覧： https://www.jsnp.org/ckd/yakuzaitoyoryo.php  \n'
+        + 'eGFR・CCrの計算： https://www.jsnp.org/egfr/')
 st.write('---------------------------------')
 st.write('■ その他')
 st.write('▸実務実習指導・管理システム  \n'
