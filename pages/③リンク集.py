263232a1a1cdaf401f0f5cc074442a3e9533a102
Ajdididi <anjokosei.druginfo@gmail.com>
2023-07-28 15:21:21 +0900

    db_update

--- pages/③リンク集.py
+++ pages/③リンク集.py
@@ -4,14 +4,14 @@ st.write('---------------------------------')
 st.write('■ DI作成資料')
 st.write('▸[添付文書改訂情報（Googleドライブ）](https://drive.google.com/drive/folders/1oWfXn2U6ntgeZ7WuOP7wXIre2AwFg7ly?usp=sharing)')
 st.write('▸[包装変更情報（Googleドライブ）](https://drive.google.com/drive/folders/1IC6nnVKAjfh28nvKBS5NTUzg6f_XYdqo?usp=sharing)')
-st.write('▸[フォーミュラリ#38（Googleドライブ](https://drive.google.com/drive/folders/1k8Nsp5Mo0DmlVAEqQiRaCQncAY3Uvv0w?usp=sharing)')
+st.write('▸[フォーミュラリ#38（Googleドライブ）](https://drive.google.com/drive/folders/1k8Nsp5Mo0DmlVAEqQiRaCQncAY3Uvv0w?usp=sharing)')
 st.write('▸相互作用エラー設定（Googleドライブ）  \n'
-        + '[ア行](https://drive.google.com/drive/folders/1C6RFutk30mCumC895SY0zRCqlWVST4QC?usp=sharing)'
-        + '[カ行～サ行](https://drive.google.com/drive/folders/1-779eNFw_1RWiizxrRug4etLGmJ2qDJZ?usp=drive_link)'
-        + '[タ行～ナ行](https://drive.google.com/drive/folders/1uesthl3SENipWn4asIlHtV71O72Qq2G0?usp=drive_link)'
-        + '[ハ行](https://drive.google.com/drive/folders/11robGV1tttS7qeiCJCxQy_JY5B7cge3p?usp=sharing)'
-        + '[マ行～ワ行](https://drive.google.com/drive/folders/1I6uv4SrbhDHv_Ow0Dkp4yN8bafkp07Jg?usp=drive_link)'
-        + '[その他項目](https://drive.google.com/drive/folders/1K3OnACCyJKT5oGWGiEAoEuuyYwmiiPFT?usp=sharing)'
+        + '・[ア行](https://drive.google.com/drive/folders/1C6RFutk30mCumC895SY0zRCqlWVST4QC?usp=sharing)  \n'
+        + '・[カ行～サ行](https://drive.google.com/drive/folders/1-779eNFw_1RWiizxrRug4etLGmJ2qDJZ?usp=drive_link)  \n'
+        + '・[タ行～ナ行](https://drive.google.com/drive/folders/1uesthl3SENipWn4asIlHtV71O72Qq2G0?usp=drive_link)  \n'
+        + '・[ハ行](https://drive.google.com/drive/folders/11robGV1tttS7qeiCJCxQy_JY5B7cge3p?usp=sharing)  \n'
+        + '・[マ行～ワ行](https://drive.google.com/drive/folders/1I6uv4SrbhDHv_Ow0Dkp4yN8bafkp07Jg?usp=drive_link)  \n'
+        + '・[その他項目](https://drive.google.com/drive/folders/1K3OnACCyJKT5oGWGiEAoEuuyYwmiiPFT?usp=sharing)  \n'
         )
 st.write('---------------------------------')
 st.write('■ 医薬品・健康食品')
@@ -30,13 +30,12 @@ st.write('---------------------------------')
 st.write('■ ガイドライン・学会HP')
 st.write('▸[NCCN](https://www.nccn.org/)')
 st.write('▸日本化学療法学会  \n'
-        + '[VCM TDMソフトウェア PAT](https://www.chemotherapy.or.jp/modules/guideline/index.php?content_id=79)'
+        + '・[VCM TDMソフトウェア PAT](https://www.chemotherapy.or.jp/modules/guideline/index.php?content_id=79)'
         )
 st.write('▸日本腎臓病薬物療法学会  \n'
-        + '[腎機能低下時に最も注意が必要な薬剤投与量一覧](https://www.jsnp.org/ckd/yakuzaitoyoryo.php)  \n'
-        + '[eGFR・CCrの計算](https://www.jsnp.org/egfr/)'
+        + '・[腎機能低下時に最も注意が必要な薬剤投与量一覧](https://www.jsnp.org/ckd/yakuzaitoyoryo.php)  \n'
+        + '・[eGFR・CCrの計算](https://www.jsnp.org/egfr/)'
         )
 st.write('---------------------------------')
 st.write('■ その他')
-st.write('▸実務実習指導・管理システム  \n'
-        + 'https://training-phm.fujifilm.com/Usersite')
+st.write('▸[実務実習指導・管理システム](https://training-phm.fujifilm.com/Usersite)')
