ba9e88f29eecfe0b5624d6641d5c6c1332776727
Ajdididi <anjokosei.druginfo@gmail.com>
2023-08-17 12:34:26 +0900

    db_update

--- pages/⑤お問い合わせ.py
+++ pages/⑤お問い合わせ.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title('お問い合わせ')
 
-st.write('このサイトに対するご意見等は下記フォームよりお寄せください。  \n'
-         + '[お問い合わせフォーム](https://forms.gle/DzHq54tLKmnPCnjv5)'
-         )
+st.write( '急ぎではない医薬品に関するお問い合わせや資料請求、  \n' 
+        + 'このサイトに対するご意見等は下記フォームよりお寄せください。  \n'
+        + '[お問い合わせフォーム](https://forms.gle/DzHq54tLKmnPCnjv5)'
+        )
