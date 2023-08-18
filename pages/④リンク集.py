5b4323ae4c2d3efe19132bf147b3015635bbc698
Ajdididi <anjokosei.druginfo@gmail.com>
2023-08-18 14:41:55 +0900

    link upload

--- pages/④リンク集.py
+++ pages/④リンク集.py
@@ -13,14 +13,30 @@ st.write('✓相互作用エラー設定（Googleドライブ）  \n'
         + '・[マ行～ワ行](https://drive.google.com/drive/folders/1I6uv4SrbhDHv_Ow0Dkp4yN8bafkp07Jg?usp=drive_link)  \n'
         + '・[その他項目](https://drive.google.com/drive/folders/1K3OnACCyJKT5oGWGiEAoEuuyYwmiiPFT?usp=sharing)  \n'
         )
+st.write('✓採用薬の院内運用 <<作成中>>')
+
 st.write('---------------------------------')
 st.write('■ 医薬品・健康食品')
 st.write('✓[PMDA医療用医薬品 情報検索](https://www.pmda.go.jp/PmdaSearch/iyakuSearch/)')
+st.write('✓[PMDA医薬品・医療機器等安全性情報](https://www.pmda.go.jp/safety/info-services/drugs/calling-attention/safety-info/0043.html)')
+st.write('✓[PMDA医薬品副作用報告受付サイト](https://www.estrigw.pmda.go.jp/Iryo/Login/Index?ReturnUrl=%2fIryo)（ユーザー名・パスワードが必要)')
 st.write('✓[SAFE-DI](https://www.safe-di.jp/)（ユーザー名・パスワードが必要)')
 st.write('✓[「健康食品」の安全性・有効性情報](https://hfnet.nibiohn.go.jp/)')
 st.write('---------------------------------')
+st.write('■ 中毒')
+st.write('✓[中毒情報センター](https://www.j-poison-ic.jp/)（ユーザー名・パスワードが必要)')
+st.write('---------------------------------')
+st.write('■ 妊婦・授乳婦')
+st.write('✓[LactMed](https://www.ncbi.nlm.nih.gov/books/NBK501922/)')
+st.write('✓[妊娠と薬情報センター](https://www.ncchd.go.jp/kusuri/)')
+st.write('---------------------------------')
+st.write('■ アンチドーピング')
+st.write('✓[global DRO](https://www.globaldro.com/JP/search/input?pls=true)')
+st.write('✓[JSPO　使用可能薬リスト](https://www.japan-sports.or.jp/medicine/doping/tabid537.html)')
+st.write('---------------------------------')
 st.write('■ 文献検索・辞書')
 st.write('✓[メディカルオンライン](https://www.medicalonline.jp/)')
+st.write('✓[医中誌](https://search.jamas.or.jp/search)')
 st.write('✓[UpToDate](https://www.uptodate.com/contents/search)')
 st.write('✓[PubMed](https://pubmed.ncbi.nlm.nih.gov/)')
 st.write('✓[ClinicalKey](https://www.clinicalkey.jp/#!/)')
@@ -37,6 +53,8 @@ st.write('✓日本腎臓病薬物療法学会  \n'
         + '・[腎機能低下時に最も注意が必要な薬剤投与量一覧](https://www.jsnp.org/ckd/yakuzaitoyoryo.php)  \n'
         + '・[eGFR・CCrの計算](https://www.jsnp.org/egfr/)'
         )
+st.write('✓[白鷺病院　透析患者に対する投薬ガイドライン](http://www.shirasagi-hp.or.jp/goda/fmly/gate.html)')
+         
 st.write('---------------------------------')
 st.write('■ その他')
 st.write('✓[実務実習指導・管理システム](https://training-phm.fujifilm.com/Usersite)')
