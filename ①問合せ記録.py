75d130554d4b3042e35c327c873bdaf7358b7c0d
Ajdididi <anjokosei.druginfo@gmail.com>
2023-09-10 13:54:33 +0900

    db_update

--- ①問合せ記録.py
+++ ①問合せ記録.py
@@ -47,11 +47,11 @@ if check_password():
         db = sqlite3.connect('toiawase.db')
         cur = db.cursor()    
 
-        cur.execute("SELECT * FROM T_old WHERE category LIKE ? OR drug LIKE ? OR question LIKE ? OR answer LIKE ?", 
+        cur.execute("SELECT * FROM T_old WHERE category LIKE ? OR drug LIKE ? OR question LIKE ? OR answer LIKE ? ORDER BY date DESC", 
                     [kensaku, kensaku, kensaku, kensaku])
         data = cur.fetchall()
 
-        cur.execute("SELECT * FROM T_new WHERE category LIKE ? OR drug LIKE ? OR question LIKE ? OR answer LIKE ? OR reference LIKE ?", 
+        cur.execute("SELECT * FROM T_new WHERE category LIKE ? OR drug LIKE ? OR question LIKE ? OR answer LIKE ? OR reference LIKE ? ORDER BY date DESC", 
                     [kensaku, kensaku, kensaku, kensaku, kensaku])
         data_n = cur.fetchall()
         cur.close()
