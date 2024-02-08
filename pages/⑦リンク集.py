25c5d8bdb9bd7187083926910568f8f77517b19c
Ajdididi <anjokosei.druginfo@gmail.com>
2024-02-08 09:23:36 +0900

    db_update

--- pages/⑦リンク集.py
+++ pages/⑦リンク集.py
@@ -13,6 +13,18 @@ st.write('✓相互作用エラー設定（Googleドライブ）  \n'
         + '・[マ行～ワ行](https://drive.google.com/drive/folders/1I6uv4SrbhDHv_Ow0Dkp4yN8bafkp07Jg?usp=drive_link)  \n'
         + '・[その他項目](https://drive.google.com/drive/folders/1K3OnACCyJKT5oGWGiEAoEuuyYwmiiPFT?usp=sharing)  \n'
         )
+st.write('✓初回チェック設定（Googleドライブ）  \n'
+        + '・[ア行](https://drive.google.com/drive/folders/1--PscdNpEABt74dwRCQo3LgnSwqTA1t1?usp=sharing)  \n'
+        + '・[カ行](https://drive.google.com/drive/folders/1MbAQAr4rtpvAfR-sHa2LF8Bcg8pfti13?usp=sharing)  \n'
+        + '・[サ行](https://drive.google.com/drive/folders/1S0HC0bV3t-8JrgfyQbcutTQqQuNvgYaE?usp=sharing)  \n'
+        + '・[タ行](https://drive.google.com/drive/folders/1WKX-Q01sWLT-OGDepdNQMDK-SE0GInbv?usp=sharing)  \n'
+        + '・[ナ行](https://drive.google.com/drive/folders/1WKX-Q01sWLT-OGDepdNQMDK-SE0GInbv?usp=sharing)  \n'
+        + '・[ハ行](https://drive.google.com/drive/folders/1dFvL5tauPqxSp3FT4PLu5ve5y6bqzYEi?usp=sharing)  \n'
+        + '・[マ行](https://drive.google.com/drive/folders/12Hm_QtdiNUKMOSlVpGwVMwBthArbmBSI?usp=sharing)  \n'
+        + '・[ヤ行](https://drive.google.com/drive/folders/1aSB7-aZLUjJHkdEcuJ9qYS-iwoh6Fzmf?usp=sharing)  \n'
+        + '・[ラ行](https://drive.google.com/drive/folders/1u8FwPXg2t4McOme3bd4gqeqTkMdLergY?usp=sharing)  \n'
+        + '・[ワ行](https://drive.google.com/drive/folders/1A70dlBVRaliC5Fq5LPlh95iO9V_4bW5L?usp=sharing)  \n'
+        )
 st.write('✓[採用薬の院内運用](https://drive.google.com/drive/folders/1tSW_ZDN1Oe19cvapkGJh6ZAd1gATtK1K?usp=drive_link)')
 
 st.write('---------------------------------')
