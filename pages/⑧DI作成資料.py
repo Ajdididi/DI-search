40e532a867982fa820e2288f3d9aaefb6876b9f5
Ajdididi <anjokosei.druginfo@gmail.com>
2024-04-10 15:43:55 +0900

    db_update

--- pages/⑧DI作成資料.py
+++ pages/⑧DI作成資料.py
@@ -1,9 +1,13 @@
 import streamlit as st
 st.title('DI作成資料')
-st.write('✓[添付文書改訂情報（Googleドライブ）](https://drive.google.com/drive/folders/1oWfXn2U6ntgeZ7WuOP7wXIre2AwFg7ly?usp=sharing)')
-st.write('✓[包装変更情報（Googleドライブ）](https://drive.google.com/drive/folders/1IC6nnVKAjfh28nvKBS5NTUzg6f_XYdqo?usp=sharing)')
-st.write('✓[フォーミュラリ#38（Googleドライブ）](https://drive.google.com/drive/folders/1k8Nsp5Mo0DmlVAEqQiRaCQncAY3Uvv0w?usp=sharing)')
-st.write('✓相互作用エラー設定（Googleドライブ）  \n'
+st.write('---------------------------------')
+st.write('■[添付文書改訂情報（Googleドライブ）](https://drive.google.com/drive/folders/1oWfXn2U6ntgeZ7WuOP7wXIre2AwFg7ly?usp=sharing)')
+st.write('---------------------------------')
+st.write('■[包装変更情報（Googleドライブ）](https://drive.google.com/drive/folders/1IC6nnVKAjfh28nvKBS5NTUzg6f_XYdqo?usp=sharing)')
+st.write('---------------------------------')
+st.write('■[フォーミュラリ#38（Googleドライブ）](https://drive.google.com/drive/folders/1k8Nsp5Mo0DmlVAEqQiRaCQncAY3Uvv0w?usp=sharing)')
+st.write('---------------------------------')
+st.write('■相互作用エラー設定（Googleドライブ）  \n'
         + '・[ア行](https://drive.google.com/drive/folders/1C6RFutk30mCumC895SY0zRCqlWVST4QC?usp=sharing)  \n'
         + '・[カ行～サ行](https://drive.google.com/drive/folders/1-779eNFw_1RWiizxrRug4etLGmJ2qDJZ?usp=drive_link)  \n'
         + '・[タ行～ナ行](https://drive.google.com/drive/folders/1uesthl3SENipWn4asIlHtV71O72Qq2G0?usp=drive_link)  \n'
@@ -11,7 +15,8 @@ st.write('✓相互作用エラー設定（Googleドライブ）  \n'
         + '・[マ行～ワ行](https://drive.google.com/drive/folders/1I6uv4SrbhDHv_Ow0Dkp4yN8bafkp07Jg?usp=drive_link)  \n'
         + '・[その他項目](https://drive.google.com/drive/folders/1K3OnACCyJKT5oGWGiEAoEuuyYwmiiPFT?usp=sharing)  \n'
         )
-st.write('✓初回チェック設定（Googleドライブ）  \n'
+st.write('---------------------------------')
+st.write('■初回チェック設定（Googleドライブ）  \n'
         + '・[ア行](https://drive.google.com/drive/folders/1--PscdNpEABt74dwRCQo3LgnSwqTA1t1?usp=sharing)  \n'
         + '・[カ行](https://drive.google.com/drive/folders/1MbAQAr4rtpvAfR-sHa2LF8Bcg8pfti13?usp=sharing)  \n'
         + '・[サ行](https://drive.google.com/drive/folders/1S0HC0bV3t-8JrgfyQbcutTQqQuNvgYaE?usp=sharing)  \n'
@@ -23,4 +28,6 @@ st.write('✓初回チェック設定（Googleドライブ）  \n'
         + '・[ラ行](https://drive.google.com/drive/folders/1u8FwPXg2t4McOme3bd4gqeqTkMdLergY?usp=sharing)  \n'
         + '・[ワ行](https://drive.google.com/drive/folders/1A70dlBVRaliC5Fq5LPlh95iO9V_4bW5L?usp=sharing)  \n'
         )
-st.write('✓[採用薬の院内運用](https://drive.google.com/drive/folders/1tSW_ZDN1Oe19cvapkGJh6ZAd1gATtK1K?usp=drive_link)')
+st.write('---------------------------------')
+st.write('■[採用薬の院内運用](https://drive.google.com/drive/folders/1tSW_ZDN1Oe19cvapkGJh6ZAd1gATtK1K?usp=drive_link)')
+st.write('---------------------------------')
