4fcfe597cc5a7c37af67c4d7f24f5cda8a429e60
Ajdididi <anjokosei.druginfo@gmail.com>
2026-09-01 17:02:14 +0900

    db_update

---
+++ pages/⑬アジア・パラ競技大会_アンチドーピング.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+st.title("アジア・パラ競技大会&nbsp;&nbsp;アンチドーピングへの対応")
+st.write("アンチドーピングに関する問合せ対応方法をまとめたページです。")
+st.write("")
+
+st.subheader("対応方法", divider="green")
+st.write("★[対応フローチャート](https://drive.google.com/file/d/18JFt16lOEr443GeJA2FtzSjYlcsMjHej/view?usp=drive_link)")
+st.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;👇")
+st.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;・[grobal DRO](https://www.globaldro.com/JP/search/input?pls=true)  \n"
+        + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;※[使い方](https://drive.google.com/file/d/1fOrmSlOgpyWEMK3zanTolqPIuSJxq3Xs/view?usp=drive_link)  \n" 
+        + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;※カルテへ文書取込みできなかった場合は、2部印刷し、1部は医師、もう一部はDIへ渡す  \n" 
+        + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;・[カルテ記載例](https://drive.google.com/file/d/1M5_eMO_khudOgTPY3ouToggZNquucW4a/view?usp=drive_link)  \n"
+        
+        + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;・TUE申請：[TUE申請の流れ／フォーマット取得](https://www.realchampion.jp/medical-staff/request/tue_flow.html)"
+         )
+st.write("")
+st.write("")
+st.write("")
+
+st.subheader("関連資料", divider="green")
+st.write("・[アンチドーピングと医療-2026年版-](https://drive.google.com/file/d/1nKHKLymT5xRrEVoLbLmHjAKDJEAQaXCq/view?usp=drive_link)")
+st.write("・[禁止表国際基準についての解説](https://drive.google.com/file/d/1nfOKNg1uQsv_hPx2Flan2ikjuaIw7ep-/view?usp=drive_link)")
+st.write("・[2026禁止表国際基準](https://www.playtruejapan.org/topics/2025/000751.html)")
+st.write("・[治療使用特例（TUE）国際基準についての解説](https://drive.google.com/file/d/1-KiaqegkV1ZWT9JwVd6wPiY7gfE8IZMW/view?usp=drive_link)")
+st.write("")
+st.write("")
+st.write("")
+
+st.subheader("AIによるサポート", divider="green")
+st.write("▻[Gemini Notebook](https://notebook.google.com/notebook/0e311acc-2f37-40cd-a28f-73682b1873a1)")
+st.write("")
+st.write("【注意点】  \n"
+        + "・Gemini Notebookに上記関連資料を読み込ませています。他の資料をアップロードしないでください。また、ソース（資料）の削除もしないでください。  \n"
+        + "・Google DIアカウントで作成しています。DIアカウントでログインしたPCからのみ使用できます。  \n"
+        + "・AIからの回答は「参考」程度として扱い、**情報の正確性を精査**してから情報提供をしてください。  \n"
+        + "  \n"
+        + "※プロンプト（AIへの指示文）の例  \n"
+        + "「明日、ソフトボールの試合を控えた選手に対して○○（医薬品名）は使用可能？」  \n"
+        + "「○○（医薬品名）を使用するための条件は？」&nbsp;&nbsp;&nbsp;←&nbsp;同じ薬でも条件によっては使用できないこともあります"
+         )
+st.write("")
+st.write("")
+st.write("")
+
+st.subheader("（対応に困った場合）専門家によるサポート", divider="green")
+st.write("大会組織委員会から貸与されたiPhoneから専門家に相談（フォーム送信）することができます。  \n"
+        + "[使用方法](https://drive.google.com/file/d/1rVwzlUQC2iQKCcHR_FY6KitgudcgMAkf/view?usp=drive_link)を確認してからご利用ください。  \n"
+        + "  \n"
+        + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;・iPhoneはDI室窓際に設置しています。使用したら必ず元の場所に戻してください。  \n"
+        + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;・iPhoneにロックはかかっていません（設定は変更しないでください）。  \n"
+        + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;・ホーム画面上の**ChatWork**というアプリからフォーム送信できます。"
+         )
